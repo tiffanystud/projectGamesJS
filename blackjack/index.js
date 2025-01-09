00c3aa460cf0d12afb075f0f0d035e7e8046e143
tiffanystud <tiffanylarsson.stud@gmail.com>
2025-01-09 14:33:21 +0100

    Update of game "blackjack".
    Functions added for functionality to create the deck, deal a card and to count the score of the cards.

--- blackjack/index.js
+++ blackjack/index.js
@@ -10,7 +10,7 @@ let dealerPoints = 0;
 let gameOver = false;
 
 const dealerCardsElem = document.querySelector(".dealerCards");
-const dealerScore =  document.querySelector(".dealerScore");
+const dealerScore = document.querySelector(".dealerScore");
 const userCardsElem = document.querySelector(".userCards");
 const userScore = document.querySelector(".userScore");
 
@@ -19,3 +19,49 @@ const standBtn = document.querySelector(".standBtn");
 const restartBtn = document.querySelector(".restartBtn");
 
 const messageElem = document.querySelector(".messageElem");
+
+// ------------- functions ------------------
+
+// För varje suit tilldelas ett av varje värde från values
+function createCardDeck() {
+    for (let suit of cardSuits) {
+        for (let value of cardValues) {
+            deckArray.push({ suit, value });
+        }
+    }
+}
+
+function dealOneCard() {
+    const drawnCard = deckArray.pop();
+    return drawnCard;
+}
+
+function scoreOfCards(cards) {
+    let score = 0;
+    let includesAce = false;
+    for (let card of cards) {
+        if (["J", "Q", "K"].includes(card.value)) {
+            score += 10;
+        } else if (card.value === "A") {
+            includesAce = true;
+            score += 11;
+        } else {
+            score += card.value;
+        }
+    }
+    // Värdet av A bestäms till 11 eller 1 beroende på score
+    if (includesAce && score > 21) {
+        score -= 10;
+    }
+    return score;
+}
+
+
+// ------------ /functions ------------------
+
+
+
+
+// ------------ interactions ------------------
+
+// ------------ /interactions ------------------
